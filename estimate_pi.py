373dca88ee1a7f0e3acc3cd928c913418d286d25
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-12-10 10:47:19 +0100

    estimate_pi.py

--- estimate_pi.py
+++ estimate_pi.py
@@ -1,4 +1,7 @@
-def count_odd_digits(n):
+def estimate_pi(num_samples):
+    """
+    Replace the code below with your own implementation.
+    """
     ### Replace with your own code (begin) ###
     pass
     ### Replace with your own code (end)   ###
